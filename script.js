19ee3c8d78e45f280a01a083e3233efc55675225
Arthur Brito <aline.nolasco.brito@gmail.com>
2023-06-23 16:55:05 -0300

    comit

---
+++ script.js
@@ -0,0 +1,44 @@
+// While
+// console.log('While');
+
+// let tabuada = 10;
+// let contador = 0;
+
+// while (contador <= 10) {
+//     let resultado = tabuada * contador;
+//     console.log(tabuada + ' x ' + contador + ' = ' + resultado);
+
+//     contador++;
+// }
+
+
+// // For
+// console.log('For');
+
+// for (let contador2 = 0; contador2 <= 10; contador2++) {
+//     let resultado = tabuada * contador2;
+//     console.log(tabuada + ' x ' + contador2 + ' = ' + resultado);
+// }
+
+
+// // Do While
+// console.log('Do While');
+
+// let contador3 = 0;
+
+// do {
+//     let resultado = tabuada * contador3;
+//     console.log(tabuada + ' x ' + contador3 + ' = ' + resultado);
+//     contador3++; 
+// } while (contador3 <=10)
+
+let valor = prompt("Digite um valor");
+let contagem = valor;
+
+while (contagem > 0 && contagem == contagem) {
+    console.log(contagem);
+    contagem--;
+}
+
+
+
